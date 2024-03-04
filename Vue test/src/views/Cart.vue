0998777a064bb4134ccfb848dd964ecb68da98c3
Samuelj58 <144460795+Samuelj58@users.noreply.github.com>
2024-03-04 07:38:34 -0500

    ww

--- Vue test/src/views/Cart.vue
+++ Vue test/src/views/Cart.vue
@@ -1,26 +1,19 @@
 <template>
   <div>
-    <h1>Your Cart</h1>
+    <h1>Cart</h1>
     <div class="products">
-      <div v-for="(product, index) in cart" :key="index">
-        <h3>{{product.name}}</h3>
-        <img :src="product.image" />
-        <div>{{product.cost}}</div>
-        <button v-on:click="removeItemFromCart(product)">Remove from cart</button>
+      <div v-for="(console, index) in cartItems" :key="index">
+        <h3>{{ console.name }}</h3>
+        <img :src="console.img" />
+        <div>{{ console.price }}</div>
+        <button @click="removeItemFromCart(console)">Remove from cart</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: ["cart"],
-  methods: {
-    removeItemFromCart(product) {
-      this.$emit("removeItemFromCart", product);
-    }
-  }
-};
+
 </script>
 
 <style>
